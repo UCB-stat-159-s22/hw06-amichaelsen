f90f7215e6b4cc77e14df005a4d461653adc67a1
Anya Michaelsen <anya.michaelsen2@gmail.com>
2022-04-20 18:28:49 +0000

    Split tests into two files, add utils.py tests

--- ligotools/tests/test_readligo.py
+++ ligotools/tests/test_readligo.py
@@ -1,5 +1,8 @@
+## Test readligo methods 
+
 from ligotools.readligo import loaddata, read_frame
 from ligotools.readligo import FileList
+
 
 def test_FileList_searchdir_data():
 	fl = FileList()
@@ -20,4 +23,3 @@ def test_loaddata_L_L1_LOSC_4_V2():
 	fn_L1 = 'data/L-L1_LOSC_4_V2-1126259446-32.hdf5'
 	strain, time, chan_dict = loaddata(fn_L1, 'H1')
 	assert len(strain) == len(time)
-
